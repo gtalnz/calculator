33a83e544f23efb654d6f7a1c7b151ba92ea3bdc
gtalnz <geoff.talbot.nz@gmail.com>
2022-07-17 01:59:24 +1200

    Implement simple functionality

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const { $ADD, $SUBTRACT, $MULTIPLY, $DIVIDE, $EQUALS, $CLEAR } = {
+	$ADD: 'add',
+	$SUBTRACT: 'subtract',
+	$MULTIPLY: 'multiply',
+	$DIVIDE: 'divide',
+	$EQUALS: 'equals',
+	$CLEAR: 'clear',
+}
+
+let displayValue = ''
+let firstValue = null
+let secondValue = null
+let selectedOperator = null
+
 const add = (num1, num2) => {
 	if (typeof num1 != 'number' || typeof num2 != 'number') {
 		throw new TypeError('Both inputs must be numbers. Did you forget to parse?')
@@ -20,19 +34,19 @@ const divide = (num1, num2) => {
 const operate = (operator, num1, num2) => {
 	let func = null
 	switch (operator) {
-		case 'add':
+		case $ADD:
 			func = add
 			break
 
-		case 'subtract':
+		case $SUBTRACT:
 			func = subtract
 			break
 
-		case 'multiply':
+		case $MULTIPLY:
 			func = multiply
 			break
 
-		case 'divide':
+		case $DIVIDE:
 			func = divide
 			break
 
@@ -40,4 +54,59 @@ const operate = (operator, num1, num2) => {
 			throw new Error(`Unrecognised operator name: ${operator}`)
 	}
 	return func(num1, num2)
+}
+
+const handleButtonClick = (e) => {
+	let target = e.target
+	let buttonId = target.id
+	if (target.classList.contains('number')) {
+		handleNumberButton(buttonId)
+	} else if (target.classList.contains('operator')) {
+		handleOperatorButton(buttonId)
+	}
+}
+
+const handleNumberButton = (numberString) => {
+	displayValue = displayValue + numberString
+	updateDisplay()
+}
+
+const handleOperatorButton = (operator) => {
+	if (operator == $CLEAR) {
+		firstValue = null
+		secondValue = null
+		selectedOperator = null
+		clear()
+		return
+	}
+	if (operator == $EQUALS) {
+		handleEquals()
+		return
+	}
+	firstValue = parseInt(displayValue)
+	selectedOperator = operator
+	secondValue = null
+	clear()
+}
+
+const clear = () => {
+	displayValue = ''
+	updateDisplay()
+}
+
+const updateDisplay = () => {
+	document.getElementById('display').innerText = displayValue
+}
+
+document.querySelectorAll('.calculator-button').forEach((button) => {
+	button.addEventListener('click', handleButtonClick)
+})
+
+const handleEquals = () => {
+	secondValue =
+		secondValue || parseInt(document.getElementById('display').innerText.trim())
+	let result = operate(selectedOperator, firstValue, secondValue)
+	displayValue = result
+	updateDisplay()
+	firstValue = result
 }
